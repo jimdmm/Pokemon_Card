3f6019b7a81c44f54337f1f7f2544187c2b57364
jimdmm <jimdouglasmmferreira@gmail.com>
2022-12-16 21:49:18 -0300

    Projeto_pokemon_card

---
+++ script.js
@@ -0,0 +1,92 @@
+const typeColor = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    eletric: "#fed330",
+    fairy: "#ff0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#80b894",
+    ground: "#efb549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "0190ff",
+};
+
+const url = " https://pokeapi.co/api/v2/pokemon/";
+const card = document.getElementById('card');
+const btn = document.getElementById('btn');
+
+let getPokeData = () => {
+    // Gerar um numnero aleatório de 1 a 150
+    let id = Math.floor(Math.random() * 150) + 1;
+    // Combinar o pokeapi url com o pokemon id
+    const finalUrl = url + id;
+
+    fetch(finalUrl)
+        .then((Response) => Response.json())
+        .then((data) => {
+            generateCard(data)
+
+        });
+}
+//Gerador de cards
+let generateCard = (data) => {
+    // obtenha os dados necessários e atribua-os às variáveis
+    const hp = data.stats[0].base_stat;
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAttack = data.stats[1].base_stat;
+    const statDefese = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+
+    // set tema de cor baseado no tipo do pokemon
+    const themeColor = typeColor[data.types[0].type.name];
+
+    card.innerHTML = `
+    <p class="hp">
+    <span>HP</span>
+    </p>
+    <img src=${imgSrc} />
+    <h2 class="poke-name">${pokeName}</h2>
+    <div class="types">
+
+    </div>
+    <div class="stats">
+        <div>
+            <h3>${statAttack}</h3>
+            <p>Attack</p>
+        </div>
+    <div>
+        <h3>${statDefese}</h3>
+        <p>Defense</p>
+    </div>
+    <div>
+        <h3>${statSpeed}</h3>
+        <p>Speed</p>
+    </div>
+    </div>
+    `;
+    appendTypes(data.types);
+    styleCard(themeColor);
+};
+let appendTypes = (types) => {
+    types.forEach((item) => {
+        let span = document.createElement("span");
+        span.textContent = item.type.name;
+        document.querySelector(".types").appendChild(span);
+    });
+};
+let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+        typeColor.style.backgroundColor = color;
+    });
+};
+
+btn.addEventListener("click", getPokeData);
+window.addEventListener("load", getPokeData);
